aac6150c0473b404806d03234ffa4b35db88a306
willpreebs <wpriebe16124@gmail.com>
2023-11-25 13:31:50 -0500

    Connect to server and get locations

--- components/home/map.js
+++ components/home/map.js
@@ -2,17 +2,38 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 
 export default function Map({ position, zoom }) {
+
+	const API = process.env.API_BASE ||  "http://localhost:4000";
+	const LOCATIONS_URL = `${API}/home/locations`;
+
+	const [locations, setLocations] = useState([]);
+
+	useEffect(() => {
+		getLocations();
+	  }, []);
+
+	const getLocations = async () => {
+		const response = await axios.get(LOCATIONS_URL);
+		setLocations(response.data);
+	}
+
 	return (
 		<MapContainer style={{ height: '100vh' }} center={position} zoom={zoom} scrollWheelZoom={true}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
-			<Marker position={position}>
-				<Popup>Snell library opa!</Popup>
-			</Marker>
+			{locations.map((location, index) => (
+				<Marker position={location.position}>
+					<Popup>{location.name}</Popup>
+				</Marker>
+			))}
 		</MapContainer>
 	);
 }
