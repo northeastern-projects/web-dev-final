2dd893204ae8809e230148f73764b8b24b64c023
RahulR100 <rahulrao@email.com>
2023-12-05 22:37:37 +0000

    initial review stack in popup

--- components/home/map.js
+++ components/home/map.js
@@ -5,27 +5,51 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import { useEffect } from 'react';
 import { useStore } from '@/contexts/store';
 import { useShallow } from 'zustand/react/shallow';
+import { Divider, Group, List, Rating, Stack, Text, Title } from '@mantine/core';
 
 export default function Map({ position, zoom }) {
 	const [locations, fetchLocations] = useStore(useShallow((state) => [state.locations, state.fetchLocations]));
-
-	const handleOnClick = (e) => {
-		console.log(e);
-	};
 
 	useEffect(() => {
 		fetchLocations();
 	}, []);
 
 	return (
-		<MapContainer style={{ height: '100vh' }} center={position} zoom={zoom} scrollWheelZoom={true} onClick={handleOnClick}>
+		<MapContainer style={{ height: '100vh' }} center={position} zoom={zoom} scrollWheelZoom={true}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 			/>
 			{locations.map((location, index) => (
 				<Marker key={index} position={[location.position.lat, location.position.lng]}>
-					<Popup>{location.name}</Popup>
+					<Popup>
+						<Title order={4} mb={15}>
+							{location.name}
+						</Title>
+						<Stack w={300} gap="xs">
+							<Group justify="space-between">
+								<Text m={0} fw={700}>
+									Username
+								</Text>
+								<Rating value={2} />
+							</Group>
+							<Text fz="sm" m={0}>
+								{location.details}
+							</Text>
+						</Stack>
+						<Divider my="sm" />
+						<Stack w={300} gap="xs">
+							<Group justify="space-between">
+								<Text m={0} fw={700}>
+									Username
+								</Text>
+								<Rating value={2} />
+							</Group>
+							<Text fz="sm" m={0}>
+								{location.details || 'No details provided'}
+							</Text>
+						</Stack>
+					</Popup>
 				</Marker>
 			))}
 		</MapContainer>
--- components/home/navbar.js
+++ components/home/navbar.js
@@ -6,11 +6,16 @@ import { useStore } from '@/contexts/store';
 export function Navbar() {
 	const { username } = useStore((state) => state.user);
 
+	const handleSearch = (e) => {
+		e.preventDefault();
+		console.log('search');
+	};
+
 	return (
 		<header className={classes.header}>
 			<Group justify="space-between" h="100%">
 				<Title order={2}>Fountain Guru</Title>
-				{username && <TextInput w={450} placeholder="Search" rightSection={<Button>Search</Button>} />}
+				{!username && <TextInput w={450} placeholder="Search" rightSection={<Button onClick={handleSearch}>Search</Button>} />}
 				{!username && (
 					<Group>
 						<Link href="/login">
